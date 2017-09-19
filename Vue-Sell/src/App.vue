fadfb15165d16d05ec870589e205011537aa3a1f
LennonChin <i@coderap.com>
2017-09-19 21:43:11 +0800

    1, the preparation of the route; 2, to achieve 1 pixel horizontal line effect;

--- Vue-Sell/src/App.vue
+++ Vue-Sell/src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评论</div>
-      <div class="tab-item">商家</div>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <a v-link="{path: '/goods'}">商品</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path: '/ratings'}">评论</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path: '/sellers'}">商家</a>
+      </div>
     </div>
-    <div class="content">
-      I am content
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
   import header from './components/header/header.vue';
+
   export default {
     components: {
       'v-header': header
@@ -22,13 +27,23 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  #app
-    .tab
-      display:flex
-      width: 100%
-      height: 40px
-      line-height 40px
-      .tab-item
-        flex: 1
-        text-align: center
+  @import "./common/stylus/mixin.styl"
+
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height 40px
+    // border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        text-decoration: none
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.active
+          color: rgb(240, 20, 20)
 </style>
--- Vue-Sell/src/main.js
+++ Vue-Sell/src/main.js
@@ -1,11 +1,31 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import sellers from 'components/sellers/sellers';
+
+import 'common/stylus/index.styl';
 
 Vue.use(VueRouter);
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+let app = Vue.extend(App);
+
+let router = new VueRouter({
+  linkActiveClass: 'active'
 });
+
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/ratings': {
+    component: ratings
+  },
+  '/sellers': {
+    component: sellers
+  }
+});
+
+router.start(app, '#app');
+router.go('/goods');
