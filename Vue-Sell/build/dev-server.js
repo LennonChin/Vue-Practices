c2f5577cf8cb5682491419dbd48b0dde9f8991e1
LennonChin <i@coderap.com>
2017-09-20 21:34:22 +0800

    1, the overall layout of the product page layout; 2, the layout of the product layout on the left side of the page; 3, the right side of the product layout layout;

--- Vue-Sell/build/dev-server.js
+++ Vue-Sell/build/dev-server.js
@@ -34,14 +34,14 @@ apiRoutes.get('/goods', function (req, res) {
   res.json({
     errno: 0,
     data: goods
-  })
+  });
 });
 
 apiRoutes.get('/ratings', function (req, res) {
   res.json({
     errno: 0,
     data: ratings
-  })
+  });
 });
 
 app.use('/api', apiRoutes);
