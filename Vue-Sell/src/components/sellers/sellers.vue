89fc54b41a5cac06c3c40edb35ddea506ddf200e
LennonChin <i@coderap.com>
2017-09-29 23:21:47 +0800

    1, develop the seller component step two; 2, design the urlParse function, design the localstorage util function;

--- Vue-Sell/src/components/sellers/sellers.vue
+++ Vue-Sell/src/components/sellers/sellers.vue
@@ -28,6 +28,10 @@
             </div>
           </li>
         </ul>
+        <div class="favorite" @click="toggleFavorite">
+          <span class="icon-favorite" :class="{'active':favorite}"></span>
+          <span class="text">{{favoriteText}}</span>
+        </div>
       </div>
       <split></split>
       <div class="bulletin">
@@ -53,6 +57,13 @@
           </ul>
         </div>
       </div>
+      <split></split>
+      <div class="info">
+        <h1 class="title border-1px">商家信息</h1>
+        <ul>
+          <li class="info-item border-1px" v-for="info in seller.infos">{{info}}</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -61,11 +72,24 @@
   import star from '../../components/star/star';
   import split from '../../components/split/split';
   import BScroll from 'better-scroll';
+  import {saveToLocal, loadFromLocal} from '../../common/js/store';
 
   export default {
     props: {
       seller: {
         type: Object
+      }
+    },
+    data() {
+      return {
+        favorite: (() => {
+          return loadFromLocal(this.seller.id, 'favorite', false);
+        })()
+      };
+    },
+    computed: {
+      favoriteText() {
+        return this.favorite ? '已收藏' : '收藏';
       }
     },
     created() {
@@ -82,6 +106,13 @@
       this._initPics();
     },
     methods: {
+      toggleFavorite(event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.favorite = !this.favorite;
+        saveToLocal(this.seller.id, 'favorite', this.favorite);
+      },
       _initScroll() {
         if (!this.scroll) {
           this.scroll = new BScroll(this.$els.seller, {
@@ -128,6 +159,7 @@
     width: 100%
     overflow: hidden
     .overview
+      position: relative;
       padding: 18px
       .title
         margin-bottom: 8px
@@ -170,6 +202,24 @@
             .stress
               font-size: 24px
 
+      .favorite
+        position: absolute
+        width: 50px
+        right: 28px
+        top: 18px
+        text-align: center
+        .icon-favorite
+          display: block
+          margin-bottom: 4px
+          color: #d4d6d9
+          font-size: 24px
+          line-height: 24px
+          &.active
+            color: rgb(240, 20, 20)
+         .text
+           line-height: 10px
+           font-size: 10px
+           color: rgb(77, 85, 93)
     .bulletin
       padding: 18px 18px 0 18px
       .title
@@ -234,5 +284,21 @@
             height: 90px
             &:last-child
               margin: 0
+
+    .info
+      padding: 18px 18px 0 18px
+      color: rgb(7, 17, 27)
+      .title
+        padding-bottom: 12px
+        line-height: 14px
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size: 14px
+      .info-item
+        padding: 16px 12px
+        line-height: 16px
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size: 12px
+        &:last-child
+          border: none
 
 </style>
