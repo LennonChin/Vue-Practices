02a36e4b9f653296abdab048c7d5a79add84b88e
LennonChin <i@coderap.com>
2017-10-11 12:17:37 +0800

    1, reload the node modules, fixed the categories' data bug;

--- online-store/src/api/api.js
+++ online-store/src/api/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 
 let host = 'http://shop.projectsedu.com';
+let local_host = 'http://127.0.0.1:8000';
 
 //获取商品类别信息
 export const queryCategorygoods = params => { return axios.get(`${host}/indexgoods/`) }
@@ -15,10 +16,10 @@ export const bannerGoods = params => { return axios.get(`${host}/banners/`) }
 //获取商品类别信息
 export const getCategory = params => {
   if('id' in params){
-    return axios.get(`${host}/categorys/`+params.id+'/');
+    return axios.get(`${local_host}/categorys/`+params.id+'/');
   }
   else {
-    return axios.get(`${host}/categorys/`, params);
+    return axios.get(`${local_host}/categorys/`, params);
   }
 };
 
